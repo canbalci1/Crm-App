220b84e4fa35ce854d2a8cb5c8dd81f127cddb59
canba <canba@CAN>
2022-10-28 12:18:02 +0300

    Add project files.

---
+++ CrmApp.WebAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrmApp.WebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
